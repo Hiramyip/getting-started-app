781ec4abbd3f08067341c0dfaad398fee830a9bc
Carlos Eduardo <lalitoceva03@hotmail.com>
2024-10-24 14:51:35 -0600

    hola chanchito feliz

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">holavillacomoestas!</p>
+                <p className="text-center">hola chanchito muy feliz</p>
             )}
             {items.map(item => (
                 <ItemDisplay
